ccc8e24b93c3d9fce8ba2cc473842a133ee90e62
Hengyu Liang <lhy1210302421@mail.ustc.edu.cn>
2021-08-18 16:24:14 +0800

    修改build过程

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 git checkout master
-git branch -d blog
+git branch -D blog
 git branch blog
 git checkout blog
 mkdocs build
 
 while read file; do
 	rm -rf $file
-done <<< `find . ! -wholename "." ! -wholename "./.git*" ! -wholename "./site*"`
+done <<< `find . ! -wholename "." ! -wholename "./.git*" ! -wholename "./site*" ! -wholename "./assets*"`
+
+cp -r ./site/* ./
+rm -rf site
 
 git add .
 git commit -m "build site"
